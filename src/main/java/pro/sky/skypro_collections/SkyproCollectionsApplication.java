d500a4d475f5269f8b422220476c1a95f62899f4
JustMonik3r <m.mintous@gmail.com>
2023-09-02 12:32:28 +0300

    skypro_collections_init

---
+++ src/main/java/pro/sky/skypro_collections/SkyproCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCollectionsApplication.class, args);
+	}
+
+}
